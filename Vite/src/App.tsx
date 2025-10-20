0f84943cd8c2e7935b6e701ef8de920fdabee5fb
climacot <fermt111@gmail.com>
2025-10-19 20:41:55 -0500

    se agrega loginca inicial de la web de reportes

--- Vite/src/App.tsx
+++ Vite/src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { supabase } from "./services/services";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      {isSignedIn ? (
+        <button
+          disabled={isLoading}
+          onClick={async () => {
+            try {
+              setIsLoading(true);
+              await supabase.auth.signOut();
+              setIsSignedIn(false);
+            } catch (error) {
+              console.error(error);
+            } finally {
+              setIsLoading(false);
+            }
+          }}
+        >
+          Salir
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      ) : (
+        <form
+          onSubmit={async (event) => {
+            try {
+              event.preventDefault();
+              const formData = new FormData(event.currentTarget);
+              setIsLoading(true);
+              const { error } = await supabase.auth.signInWithPassword({
+                email: formData.get("username")!.toString(),
+                password: formData.get("password")!.toString(),
+              });
+              if (error) throw new Error(error.message);
+              setIsSignedIn(true);
+            } catch (error) {
+              console.error(error);
+            } finally {
+              setIsLoading(false);
+            }
+          }}
+        >
+          <input type="email" name="username" placeholder="username" />
+          <input type="password" name="password" placeholder="password" />
+          <button disabled={isLoading} type="submit">
+            Entrar
+          </button>
+        </form>
+      )}
+      {isSignedIn && (
+        <button
+          disabled={isLoading}
+          onClick={async () => {
+            try {
+              setIsLoading(true);
 
-export default App
+              const {
+                data: { session },
+              } = await supabase.auth.getSession();
+
+              await fetch("http://127.0.0.1:8000", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${session!.access_token}`,
+                },
+              });
+            } catch (error) {
+              console.error(error);
+            } finally {
+              setIsLoading(false);
+            }
+          }}
+        >
+          Procesar recursos
+        </button>
+      )}
+    </div>
+  );
+};
