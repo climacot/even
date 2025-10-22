a4bfa1dcd476e00e4430c121daac998d21b3e365
climacot <fermt111@gmail.com>
2025-10-22 13:08:50 -0500

    agrego .env variables

--- Vite/src/services/services.ts
+++ Vite/src/services/services.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import { QueryClient } from "@tanstack/react-query";
 
 export const supabase = createClient(
-  "https://hgsxnscedselcjiucliu.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhnc3huc2NlZHNlbGNqaXVjbGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2MDUwMTMsImV4cCI6MjA3NTE4MTAxM30.Bz8FvRMIoBEeLLfwiZYV__29a9DTcn9uSxXgbBBHOuc"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
 export const queryClient = new QueryClient();
