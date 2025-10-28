e37417e20da6f09132a6639dcbd3c672d1a47cd3
climacot <fermt111@gmail.com>
2025-10-28 13:18:06 -0500

    agrego logo de la universidad

--- Electron/src/components/logo.tsx
+++ Electron/src/components/logo.tsx
@@ -1,6 +1,11 @@
 export const Logo = () => (
-  <div className="font-bold text-4xl">
-    <span className="text-blue-600">EV</span>
-    <span className="text-yellow-500">EN</span>
+  <div className="font-bold text-4xl flex justify-between items-center">
+    <div>
+      <span className="text-blue-600">EV</span>
+      <span className="text-yellow-500">EN</span>
+    </div>
+    <div>
+      <img src="unicauca.svg" width="50" height="50" />
+    </div>
   </div>
 );
